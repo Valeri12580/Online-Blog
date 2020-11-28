5dea8c41ace4adafc811652c2339be20136c6b2c
Valeri <valeri125we@gmail.com>
2020-11-28 13:37:53 +0200

    up

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeUnauthenticatedComponent } from './home-unauthenticated/home-unauthenticated.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { ArticlesListComponent } from './articles-list/articles-list.component';
-import { ArticleComponent } from './article/article.component';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {FooterComponent} from './footer/footer.component';
+import {HomeUnauthenticatedComponent} from './home-unauthenticated/home-unauthenticated.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {ArticlesListComponent} from './articles-list/articles-list.component';
+import {ArticleComponent} from './article/article.component';
+import {RouterModule} from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {ShopComponent} from './shop/shop.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +22,37 @@ import { ArticleComponent } from './article/article.component';
     RegisterComponent,
     LoginComponent,
     ArticlesListComponent,
-    ArticleComponent
+    ArticleComponent,
+    AboutComponent,
+    ShopComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeUnauthenticatedComponent
+      },
+      {
+        path: 'articles',
+        component: ArticlesListComponent
+      },
+      {
+        path: 'users/register',
+        component: RegisterComponent
+      }, {path: 'users/login', component: LoginComponent},
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'shop',
+        component: ShopComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
